b4273c387998edccb79c8628f83d26f2349c0f3f
Belogortsev Denis <belogortsev_da@mail.ru>
2025-02-22 17:50:17 +0500

    vse peredelal

--- obuch2.c
+++ obuch2.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
+
 int main(void)
 {
-    char symvol = 'A';
-    float pi = 3.141592;
-    unsigned short age = 41;
-    printf("Привет, меня зовут Денис. Мой возраст %d \n", age);
-    printf("Введите свой возраст: \n");
-    scanf("%d", &age);
-    printf("Отныне Ваш возраст: %d \n", age);
-    printf("Символ %c в кодировке имеет значение ", symvol);
-    printf("%d \n", symvol);
-    printf("Число Пи = %3.2f \n", pi);
-    printf("int %lu \n", sizeof(int));
-    printf("short %lu \n", sizeof(short));
-    printf("unsigned short %lu \n", sizeof(age));
-    return(0);    
+    int param1 = 15, param2 = 18, param3 = 18;
+    int max = param3;
+    if (param1 > param2 && param1 > param3)
+    {
+        max = param1;
+    }
+    else if (param2 > param1 && param2 > param3)
+        {
+            max = param2;
+        }
+    printf("MAX %d is \n",max);
+    return(0);
 }
-
